7229503c3a1958d601c609b2fcbfe1f9f33acad6
aparna109 <aparnagurajarapu@gmail.com>
2025-09-09 11:51:57 +0530

    hello

---
+++ class.java.java
@@ -0,0 +1,35 @@
+class Student{
+
+//private data members
+private String name;
+private int age;
+
+//getter and setter for name
+public string get name(){
+return name;
+}
+public void set name(String name){
+this.name=name;
+}
+//getter and setter for age
+public int get age(){
+return age;
+}
+public void set age (int age){
+this. age=age;
+}
+}
+public class encapsulation demo{
+public static void main(String args[]) {
+student s= new student();
+
+//setting values using setter methods
+s.setname("Sonia");
+s.setage(19);
+
+//getting values using getter methods
+system.out.println("name:"+ s.getName());
+system.out.println('age;'+ s.getage());
+}
+}
+
